ba0406a1f3c62e37ae7e93cdbac8338f7b527cc4
Laudemir <nlaudemir@gmail.com>
2021-08-09 14:48:06 -0300

    Criação de tabuleiro

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container");
+
+const tabuleiro = () => {
+  for (let i = 0; i < 7; i++) {
+    let linha = document.createElement("div");
+    linha.classList.add("linha", `linha_${i}`);
+    container.appendChild(linha);
+    for (let j = 0; j < 6; j++) {
+      let coluna = document.createElement("div");
+      coluna.classList.add("bloco", `bloco_${j}`);
+      linha.appendChild(coluna);
+    }
+  }
+};
+
+tabuleiro();
