28c9c93971c9e044fe64a44b4a5d8b79c22b4efb
Laudemir <nlaudemir@gmail.com>
2021-08-09 18:59:53 -0300

    Primeiros passos dados para a criação da função de validação

--- script.js
+++ script.js
@@ -24,3 +24,40 @@ jogadores.appendChild(jogador_1);
 const jogador_2 = document.createElement("div");
 jogador_2.classList.add("jogador_2");
 jogadores.appendChild(jogador_2);
+
+let arrTabuleiro = [
+  ["P", "P", "P", "P", "V", "V", "V"],
+  ["P", "V", "V", "V", "V", "V", "V"],
+  ["P", "V", "V", "V", "P", "V", "V"],
+  ["V", "V", "V", "V", "P", "V", "V"],
+  ["V", "V", "V", "P", "P", "P", "V"],
+  ["V", "V", "V", "V", "P", "V", "V"],
+];
+const vitoria = () => {
+  for (let i = 0; i < arrTabuleiro.length; i++) {
+    for (let j = 0; j < arrTabuleiro[0].length - 3; j++) {
+      if (
+        arrTabuleiro[i][j] === arrTabuleiro[i][j + 1] &&
+        arrTabuleiro[i][j + 1] === arrTabuleiro[i][j + 2] &&
+        arrTabuleiro[i][j + 2] === arrTabuleiro[i][j + 3] &&
+        arrTabuleiro[i][j] === "P"
+      ) {
+        console.log("test_0");
+      }
+    }
+  }
+  for (let i = 0; i < arrTabuleiro.length - 3; i++) {
+    for (let j = 0; j < arrTabuleiro[0].length; j++) {
+      if (
+        arrTabuleiro[i][j] === arrTabuleiro[i + 1][j] &&
+        arrTabuleiro[i + 1][j] === arrTabuleiro[i + 2][j] &&
+        arrTabuleiro[i + 2][j] === arrTabuleiro[i + 3][j] &&
+        arrTabuleiro[i][j] === "P"
+      ) {
+        console.log("test_1");
+      }
+    }
+  }
+};
+
+vitoria();
