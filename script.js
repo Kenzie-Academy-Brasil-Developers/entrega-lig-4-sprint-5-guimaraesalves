262b88a6027caeabed90b475d280556073ae4c6a
Mateus Alves <guimaraesalves45@gmail.com>
2021-08-09 17:31:45 -0300

    commit alteração cor e tamnho do disco addEventListener

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const tabuleiro = () => {
 
 tabuleiro();
 
+
 const jogadores = document.querySelector(".jogadores");
 
 const jogador_1 = document.createElement("div");
@@ -24,3 +25,42 @@ jogadores.appendChild(jogador_1);
 const jogador_2 = document.createElement("div");
 jogador_2.classList.add("jogador_2");
 jogadores.appendChild(jogador_2);
+
+
+
+//Inicializando eventos Selecionando discos
+
+    
+
+    jogador_1.addEventListener("click", function(event){
+    jogador_1.style.width = "40px";
+    jogador_1.style.height = "40px";
+    });       
+   
+    jogador_1.addEventListener("mouseover", function(event){
+        jogador_1.style.backgroundColor = "#3b3b2f";
+    });
+    jogador_1.addEventListener("mouseout", function(event){
+        jogador_1.style.backgroundColor = "#000";
+    });
+
+
+
+    
+    jogador_2.addEventListener("click", function(event){
+    jogador_2.style.width = "40px";
+    jogador_2.style.height = "40px";            
+    });      
+   
+    jogador_2.addEventListener("mouseover", function(event){
+        jogador_2.style.backgroundColor = "#e64f47";
+    });
+    jogador_2.addEventListener("mouseout", function(event){
+        jogador_2.style.backgroundColor = "red";
+    });
+
+
+
+
+
+
