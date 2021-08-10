7b549659606f34241e28fa5fa6a98e046bcf9709
Felipe Silveira <felipelarson@msn.com>
2021-08-10 08:52:40 +0100

    refatorando codigo jogador

--- script.js
+++ script.js
@@ -31,12 +31,7 @@ const criarJogador = (jogador, strJogador) => {
 }
 
 const jogador_1 = document.createElement("div");
-// jogador_1.classList.add("jogador_1");
-// jogadores.appendChild(jogador_1);
-
 const jogador_2 = document.createElement("div");
-// jogador_2.classList.add("jogador_2");
-// jogadores.appendChild(jogador_2);
 
 criarJogador(jogador_1, 'jogador_1')
 criarJogador(jogador_2, 'jogador_2')
