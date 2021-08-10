af38129bd8c3bf13228f43ae40cb1f0cd7d7f7d0
Felipe Silveira <felipelarson@msn.com>
2021-08-10 11:06:38 +0100

    dividido em funções tabuleiro

--- script.js
+++ script.js
@@ -3,18 +3,26 @@
 //================================
 
 const tabuleiro = () => {
+  criaLinhas()
+};
+
+const criaLinhas = () => {
   const container = document.querySelector(".container");
   for (let i = 0; i < 7; i++) {
     let linha = document.createElement("div");
     linha.classList.add("linha", `linha_${i}`);
     container.appendChild(linha);
-    for (let j = 0; j < 6; j++) {
-      let coluna = document.createElement("div");
-      coluna.classList.add("bloco", `bloco_${j}`);
-      linha.appendChild(coluna);
-    }
+    criaColunas(linha)
   }
-};
+}
+
+const criaColunas = (linha) => {
+  for (let j = 0; j < 6; j++) {
+    let coluna = document.createElement("div");
+    coluna.classList.add("bloco", `bloco_${j}`);
+    linha.appendChild(coluna);
+  }
+}
 
 tabuleiro();
 
