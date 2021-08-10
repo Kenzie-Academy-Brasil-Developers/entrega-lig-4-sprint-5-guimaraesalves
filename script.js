6e04deb8df728114bb3292a62a04a569c73b4278
Felipe Silveira <felipelarson@msn.com>
2021-08-10 20:36:43 +0100

    alteração das cores invertidades

--- script.js
+++ script.js
@@ -31,10 +31,10 @@ let jogadorAtual = player1;
 // alternância de jogadores
 container.addEventListener("click", function (event) {
   if (jogadorAtual === player1) {
-    jogador.style.backgroundColor = "red";
+    jogador.style.backgroundColor = "black";
     jogadorAtual = player2; //volta para true bloqueando o clique do jogador 2;
   } else {
-    jogador.style.backgroundColor = "black";
+    jogador.style.backgroundColor = "red";
     jogadorAtual = player1;
   }
 });
@@ -56,12 +56,10 @@ for (let i = colunas.length - 1; i >= 0; i--) {
     let indice = arrTabuleiro[i].lastIndexOf("V");
     arrTabuleiro[i][indice] = jogadorAtual;
     //colocar a cor do disco no local do tabuleiro
-    console.log(arrTabuleiro);
     let estilo = `.linha_${[i]} > .bloco_${[indice]}`;
     let cor = document.querySelector(estilo);
     if (jogadorAtual === "B") {
       cor.style.backgroundColor = "black";
-      console.log(estilo);
     } else {
       cor.style.backgroundColor = "red";
     }
