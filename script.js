b2944910444ee1d451fcf12e02e423f5ad7f7907
Laudemir <nlaudemir@gmail.com>
2021-08-11 17:52:44 -0300

    função cronometro

--- script.js
+++ script.js
@@ -199,3 +199,25 @@ container.addEventListener("click", function () {
     jogador.classList.remove("chutar");
   }, 700);
 });
+
+function cronometro(duration, display) {
+  let timer = duration,
+    minutes,
+    seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    display.textContent = minutes + ":" + seconds;
+    if (++timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+
+window.onload = function () {
+  let duration = 0;
+  display = document.querySelector(".cronometro");
+  cronometro(duration, display);
+};
