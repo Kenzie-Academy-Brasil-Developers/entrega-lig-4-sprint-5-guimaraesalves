449a49d55f5add74edf57cb5ac1b084ddda63e84
Felipe Silveira <felipelarson@msn.com>
2021-08-11 17:32:19 +0100

    Criacao trofeu

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function criaCardVitoria(jogador) {
   const cardVitoria = document.createElement("div");
   cardVitoria.classList.add("cardVitoria")
-  cardVitoria.innerText = `${jogador}`
+  criarTitulo(cardVitoria, jogador)
   document.body.appendChild(cardVitoria)
 }
+
+function criarTitulo(cardVitoria, titulo) {
+  const criarTitulo = document.createElement("div");
+  criarTitulo.classList.add("criarTitulo")
+  criarTitulo.innerText = `${titulo}`
+  cardVitoria.appendChild(criarTitulo)
+
+  criaTrofeu(cardVitoria)
+}
+
+function criaTrofeu(cardVitoria) {
+  const criaTrofeu = document.createElement("img");
+  criaTrofeu.classList.add("criaTrofeu")
+  cardVitoria.appendChild(criaTrofeu)
+}
+
 
 criaCardVitoria("Palmeiras tem Mundial")
