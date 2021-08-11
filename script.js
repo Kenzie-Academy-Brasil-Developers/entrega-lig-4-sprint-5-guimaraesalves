2a29342d7a3398b1aec32011e6825ed54f77370e
Felipe Silveira <felipelarson@msn.com>
2021-08-11 16:17:58 +0100

    eliminando codigo extra

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-const container = document.querySelector(".container");
-
-const tabuleiro = () => {
-  for (let i = 0; i < 7; i++) {
-    let linha = document.createElement("div");
-    linha.classList.add("linha", `linha_${i}`);
-    container.appendChild(linha);
-    for (let j = 0; j < 6; j++) {
-      let coluna = document.createElement("div");
-      coluna.classList.add("bloco", `bloco_${j}`);
-      linha.appendChild(coluna);
-    }
-  }
-};
-
-tabuleiro();
-
-const jogadores = document.querySelector(".jogadores");
-
-const jogador_1 = document.createElement("div");
-jogador_1.classList.add("jogador_1");
-jogadores.appendChild(jogador_1);
-
-const jogador_2 = document.createElement("div");
-jogador_2.classList.add("jogador_2");
-jogadores.appendChild(jogador_2);
-
 function criaCardVitoria(jogador) {
   const cardVitoria = document.createElement("div");
   cardVitoria.classList.add("cardVitoria")
-  cardVitoria.innerText = jogador
+  cardVitoria.innerText = `${jogador}`
   document.body.appendChild(cardVitoria)
 }
 
