504d0d6c83559aa5fcdcf30f7274bbcfce8f8d0d
Laudemir <nlaudemir@gmail.com>
2021-08-11 22:34:25 -0300

    Coreções nas animações e refatoração do codigo de timer

--- script.js
+++ script.js
@@ -207,24 +207,18 @@ container.addEventListener("click", function () {
   }, 2000);
 });
 
-function cronometro(duration, display) {
-  let timer = duration,
-    minutes,
-    seconds;
-  setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    display.textContent = minutes + ":" + seconds;
-    if (--timer < 0) {
-      timer = duration;
-    }
-  }, 1000);
-}
+const minutos = 0;
+let tempo = minutos * 60;
 
-window.onload = function () {
-  let duration = 300;
-  display = document.querySelector(".cronometro");
-  cronometro(duration, display);
-};
+const cronometro = document.querySelector(".cronometro");
+
+setInterval(atualiza, 1000);
+
+function atualiza() {
+  const minutos = Math.floor(tempo / 60);
+  let segundos = tempo % 60;
+
+  segundos = segundos < 10 ? "0" + segundos : segundos;
+  cronometro.innerHTML = `${minutos}:${segundos}`;
+  tempo++;
+}
